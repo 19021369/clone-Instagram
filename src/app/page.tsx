760feb3a3ece1b85152f2d955dc0d96153b6dbc4
Ngô Quang Thiện <gamehay.net12321@gmail.com>
2023-07-19 08:37:07 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Intro } from '../components'
+
+export default function Home() {
+  return (
+    <main className="h-screen">
+    <Intro />   
+    </main>
+  )
+}
